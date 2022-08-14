a827ad5f3e53e8265d895647a40a00c51d284ded
havoc-nessity <111239958+havoc-nessity@users.noreply.github.com>
2022-08-14 16:43:04 +0000

    first saved version
    dont worry theres more

---
+++ Coffee_Shop.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+#suck my asshole
+
+print("\nWelcome to the coffee shop, I'm your robot server, and no, not that kind of server, the kind that serves you coffee.\n")
+order = input("So, what would you like?\n").lower().strip()
+
+if order == "help":
+
+elif order == "menu":
+
+elif order == "dark coffee":
+
+elif order == "mocha":
+
+elif order == "iced coffee":
+
+else:
+	print("dafaq? fuck off mate")
+	exit()
+
+print("okay you'll get your " + order + " shortly.")
+
+
