adf25f60427b2e3938091904638be14ea0d9382a
havoc-nessity <111239958+havoc-nessity@users.noreply.github.com>
2022-08-14 16:52:00 +0000

    a better version
    with more things

--- Coffee_Shop.py
+++ Coffee_Shop.py
@@ -1,24 +1,42 @@
 #!/usr/bin/env python3
 
-#suck my asshole
+#suck my arse
 
-print("\nWelcome to the coffee shop, I'm your robot server, and no, not that kind of server, the kind that serves you coffee.\n")
-order = input("So, what would you like?\n").lower().strip()
+def main():
 
-if order == "help":
+	import time
 
-elif order == "menu":
+	print("\n\"Welcome to the coffee shop, I'm your robot server, and no, not that kind of server, the kind that serves you coffee.\"\n")
+	order=input('"So, what would you like?"\n').lower().strip()
 
-elif order == "dark coffee":
+	menu_list=["Dark Coffee", "Iced Coffee", "Hot Coffee",]
 
-elif order == "mocha":
+	ans=0
+	
+#fuck this "while" bullshit took me so damn long just to loop this part
+	while(ans==0):
 
-elif order == "iced coffee":
+		if order=="help":
+			time.sleep(0.200)
+			print("\n\"If you want to do an action, you can add a '*' symbol at the beggining and end of the said action")
 
-else:
-	print("dafaq? fuck off mate")
-	exit()
+		elif order=="menu":
+			print("\n")
+			print(menu_list)
+			order=input("\n").lower().strip()
 
-print("okay you'll get your " + order + " shortly.")
+		elif order=="dark coffee":
+			ans=1
 
-
+		#elif order == "iced coffee":
+			
+		else:
+			order=input("\nwhat? I can't seem to understand...\n").lower().strip()
+			
+	print("\n\"Okay, you'll get your "+order+" shortly...\"")
+	
+if __name__=="__main__":
+	main()
+	
+exit()
+main()
